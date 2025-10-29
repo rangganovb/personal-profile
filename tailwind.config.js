4e54fd9d53c0171eb8cf752a71e0ad59b6839459
rangganovb <ranggamum@gmail.com>
2025-10-29 22:34:42 +0700

    Menyesuaikan konfigurasi agar warna dapat diterapkan

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
+      // Warna Utama dari Desain Figma
+      colors: {
+        "dark-blue": "#232946",
+        "grayish-blue": "#5A5F73",
+        "primary-purple": "#7755FF",
+        "primary-pink": "#FF54B0",
+        "border-gray": "#E5E7EB",
+      },
+      backgroundImage: {
+        "hero-gradient": "linear-gradient(90deg, #E0EAFE 0%, #F9E5FF 100%)",
+        "btn-gradient": "linear-gradient(90deg, #7755FF 0%, #FF54B0 100%)",
+      },
+    },
   },
   plugins: [],
 };
